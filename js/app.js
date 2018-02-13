e3c18be7de5a8461b9a91a9290a13d53c7e88e48
Eleyka <eleynekrd@gmail.com>
2018-02-13 09:07:27 -0500

    probando

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('search-form');
+const searchField = document.getElementById('search-keyword');
+const responseContainer = document.getElementById('response-container');
+let searchedForText;
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  responseContainer.innerHTML = '';
+  searchedForText = searchField.value;
+  getNews();
+});
